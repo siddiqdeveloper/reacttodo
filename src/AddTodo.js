0e895257315b97b87e57b7b8689a42d3cd29d156
siddiq <asiddiq538@gmail.com>
2024-04-18 08:00:04 +0530

    first commit

---
+++ src/AddTodo.js
@@ -0,0 +1,17 @@
+
+
+const AddTodo = () =>{
+    return <>
+     <div class="container">
+        <div class="form">
+            <input type="text" class="input" />
+            <input type="submit" class="add" value="Add Task" />
+        </div>
+     
+        </div>
+    
+
+    </>
+}
+
+export default AddTodo;
